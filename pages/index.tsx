2db37b6bb9e42c0b52c270d1de969d0ae625ba7a
madan95 <madanlimbu.dev@gmail.com>
2020-12-06 00:28:41 +0000

    Testing google adsense

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+
 export async function getStaticProps() {
     return { props: { name: 'madan' } }
 }
@@ -5,6 +7,11 @@ export async function getStaticProps() {
 function IndexPage({ name }) {
     return (
         <>
+        <Head>
+        <meta property="og:title" content="My page title" key="title" />
+        <title>Testing Head component</title>    
+        <script data-ad-client="ca-pub-7420131458491934" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        </Head>
         {name}
             <div>Test home page.</div>
         </>);
