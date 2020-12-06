282783e4b2eba2d3c714d9fb3dbb86d84602908c
madan95 <madanlimbu.dev@gmail.com>
2020-12-06 04:38:53 +0000

    remove statics as we don't need it

--- docs/_next/static/WJvN3CpfRonG90eS5SjP0/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = (function(a){return {__rewrites:[{source:"\u002F:path*",destination:a}],"/":["static\u002Fchunks\u002Fpages\u002Findex-3c3760868d38342e6b5c.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-470541d59428bd0a4df4.js"],sortedPages:[a,"\u002F_app","\u002F_error"]}}("\u002F"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
