d1cb7f741546a4a055f182c34e9f491559cb3778
Madan Limbu <madanlimbu.dev@gmail.com>
2021-05-30 14:20:51 +0100

    Don't call for new listing on first load

--- components/Posts/index.tsx
+++ components/Posts/index.tsx
@@ -1,4 +1,4 @@
-import { ReactElement, useEffect, useState } from "react"
+import { ReactElement, useEffect, useState, useRef } from "react"
 import api from "../../lib/api/client";
 import { PostContentType } from "../../lib/api/contentful/interface";
 import PostCard from "../PostCard";
@@ -20,6 +20,7 @@ export default function Posts(props: PostsProps): ReactElement {
     const [offset, setOffset] = useState<number>(props.offset);
     const [total, setTotal] = useState<number>(props.total);
     const [hasMore, setHasMore] = useState<boolean>(props.hasMore);
+    const notInitialRender = useRef(false);
 
     useEffect(() => {
         console.log(`Post props`, props);
@@ -30,16 +31,20 @@ export default function Posts(props: PostsProps): ReactElement {
     }
 
     useEffect(() => {
-        api.getPosts({
-            skip: offset,
-            limit: SIZE,
-            preview: false,
-        }).then(postCollection => {
-            setPostList((currentPostList) => [...currentPostList, ...postCollection.postCollection.items]);
-            setTotal(postCollection.postCollection.total);
-            setCurrent(offset + SIZE);
-            setHasMore(postCollection.postCollection.total > (current + SIZE));
-        });
+        if (notInitialRender.current) {
+            api.getPosts({
+                skip: offset,
+                limit: SIZE,
+                preview: false,
+            }).then(postCollection => {
+                setPostList((currentPostList) => [...currentPostList, ...postCollection.postCollection.items]);
+                setTotal(postCollection.postCollection.total);
+                setCurrent(offset + SIZE);
+                setHasMore(postCollection.postCollection.total > (current + SIZE));
+            });
+        } else {
+            notInitialRender.current = true;
+        }
     }, [offset]);
 
     return (
